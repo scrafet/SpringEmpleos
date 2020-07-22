3404298813a334e636b53dba876e92f21a57ab58
Scrafet <Scrafet@gmail.com>
2020-07-21 23:09:06 -0500

    usando th:each para mostrar la lista en home.html e integrandolo con el diseño html

--- src/main/java/net/scrafet/controller/HomeController.java
+++ src/main/java/net/scrafet/controller/HomeController.java
@@ -54,19 +54,8 @@ public class HomeController {
 	
 	@GetMapping("/")
 	public String mostrarHome(Model model) {
-		/*
-		model.addAttribute("mensaje", "Bienvenidos a Empleos APP");
-		model.addAttribute("fecha", new Date());
-		*/
-		String nombre = "Auxiliar de Contabilidad"; 
-		Date fechaPub = new Date();
-		Double salario=9000.0;
-		boolean vigente=true;
-		
-		model.addAttribute("nombre", nombre);
-		model.addAttribute("fecha", fechaPub);
-		model.addAttribute("salario", salario);
-		model.addAttribute("vigente", vigente);
+		List<Vacante> lista = serviceVacantes.buscarTodas();
+		model.addAttribute("vacantes", lista);
 		
 		return "home";
 		
