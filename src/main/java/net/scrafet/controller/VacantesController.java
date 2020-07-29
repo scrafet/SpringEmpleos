d2917a23cd2e69dc69dd1e1c3327558f01770130
Scrafet <Scrafet@gmail.com>
2020-07-28 19:48:36 -0500

    se agrego databinding() , initbindin() para fechas y formatos de html

--- src/main/java/net/scrafet/controller/VacantesController.java
+++ src/main/java/net/scrafet/controller/VacantesController.java
@@ -1,10 +1,17 @@
 package net.scrafet.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -14,28 +21,62 @@ import net.scrafet.service.IVacantesService;
 @Controller
 @RequestMapping("/vacantes")
 public class VacantesController {
-	
+
 	@Autowired
-	private IVacantesService serviceVacantes; 
-	
+	private IVacantesService serviceVacantes;
+
+	@GetMapping("/create")
+	public String crear() {
+		return "vacantes/formVacante";
+	}
+
+	@PostMapping("/save")
+	public String guardar(Vacante vacante) {
+		System.out.println("vacante : " + vacante);
+		return "vacantes/listVacantes";
+	}
+//	
+//	@PostMapping("/save")
+//	public String guardar(@RequestParam("nombre") String nombre,
+//			@RequestParam("descripcion") String descripcion,
+//			@RequestParam("estatus") String estatus,
+//			@RequestParam("fecha") String fecha,
+//			@RequestParam("destacado") int destacado,
+//			@RequestParam("salario") double salario,
+//			@RequestParam("detalles") String detalles) {
+//		System.out.println("Nombre vacante : " + nombre);
+//		System.out.println("Descripcion : " + descripcion);
+//		System.out.println("Estatus : " + estatus);
+//		System.out.println("Fecha Publicacion : " + fecha);
+//		System.out.println("Destacado : " + destacado);
+//		System.out.println("Salario Ofrecido : " + salario);
+//		System.out.println("detalles : " + detalles);		
+//		return "vacantes/listVacantes";
+//	}
+
 	@GetMapping("/view/{id}")
 	public String verDetalle(@PathVariable("id") int idVacante, Model model) {
-		Vacante vacante= serviceVacantes.buscarPorId(idVacante);
-		
-		System.out.println("vacante : " +  vacante);
+		Vacante vacante = serviceVacantes.buscarPorId(idVacante);
+
+		System.out.println("vacante : " + vacante);
 		model.addAttribute("vacante", vacante);
-		
-		//Buscar los datos de la vacante en la base de datos
-		
-		
+
+		// Buscar los datos de la vacante en la base de datos
+
 		return "detalle";
 	}
-	
+
 	@GetMapping("/delete")
-	public String eliminar(@RequestParam("id")int idVacante,Model model) {
-		System.out.println("borrando vacante con id : " + idVacante );
-		model.addAttribute("id", idVacante );
+	public String eliminar(@RequestParam("id") int idVacante, Model model) {
+		System.out.println("borrando vacante con id : " + idVacante);
+		model.addAttribute("id", idVacante);
 		return "mensaje";
+	}
+
+	@InitBinder
+	public void initBinder(WebDataBinder webDataBinder) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+		webDataBinder.registerCustomEditor(Date.class, new CustomDatEditor(dateFormat, false));
 	}
 
 }
--- src/main/java/net/scrafet/model/Vacante.java
+++ src/main/java/net/scrafet/model/Vacante.java
@@ -11,6 +11,8 @@ public class Vacante {
 	private Double salario;
 	private Integer destacado;
 	private String imagen="no-image.png";
+	private String estatus;
+	private String detalles;
 	
 	
 	public Integer getDestacado() {
@@ -56,11 +58,27 @@ public class Vacante {
 	public void setImagen(String imagen) {
 		this.imagen = imagen;
 	}
+	
+	
+	public String getEstatus() {
+		return estatus;
+	}
+	public void setEstatus(String estatus) {
+		this.estatus = estatus;
+	}
+	public String getDetalles() {
+		return detalles;
+	}
+	public void setDetalles(String detalles) {
+		this.detalles = detalles;
+	}
 	@Override
 	public String toString() {
 		return "Vacante [id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + ", fecha=" + fecha
-				+ ", salario=" + salario + ", destacado=" + destacado + "]";
+				+ ", salario=" + salario + ", destacado=" + destacado + ", imagen=" + imagen + ", estatus=" + estatus
+				+ ", detalles=" + detalles + "]";
 	}
+	
 	
 	
 	
