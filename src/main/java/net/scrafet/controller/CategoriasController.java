bb5e5495f1fce6f3edcf08909272617adaa02e13
Scrafet <Scrafet@gmail.com>
2020-08-01 20:32:10 -0500

    exer 2

--- src/main/java/net/scrafet/controller/CategoriasController.java
+++ src/main/java/net/scrafet/controller/CategoriasController.java
@@ -1,18 +1,35 @@
 package net.scrafet.controller;
 
+import java.util.List;
+
+import javax.smartcardio.ATR;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import net.scrafet.model.Categoria;
+import net.scrafet.model.Vacante;
+import net.scrafet.service.ICategoriasService;
 
 @Controller
 @RequestMapping(value="/categorias")
 public class CategoriasController {
+	
+	@Autowired
+	private ICategoriasService serviceCategorias;
 
 	// @GetMapping("/index")
 	@RequestMapping(value="/index", method=RequestMethod.GET)
-	public String mostrarIndex(Model model) {   
+	public String mostrarIndex(Model model) {
+		List<Categoria> lista=serviceCategorias.buscarTodas();
+		model.addAttribute("categorias",lista);
 	return "categorias/listCategorias";
 	}
 	// @GetMapping("/create")
@@ -22,9 +39,17 @@ public class CategoriasController {
 	}
 	// @PostMapping("/save")
 	@RequestMapping(value="/save", method=RequestMethod.POST)
-	public String guardar(@RequestParam("nombre") String nombre,@RequestParam("descripcion") String descripcion ) {
-		System.out.println("Categoria : " + nombre);
-		System.out.println("Descripcion : " + descripcion);
-	return "categorias/listCategorias";
+	public String guardar(Categoria categoria, BindingResult result, RedirectAttributes attributes) {
+		if (result.hasErrors()) {
+			for (ObjectError error : result.getAllErrors()) {
+				System.out.println("Ocurrio un Error : " + error.getDefaultMessage());
+			}
+			return "categorias/listCategorias";
+		}
+	serviceCategorias.guardar(categoria);
+	attributes.addFlashAttribute("msg","se guardo Correctamente");
+	System.out.println("Categoria : " + categoria);
+	return "redirect:/categorias/index";
 	}
+	
 }
---
+++ src/main/java/net/scrafet/service/CategoriasServiceImpl.java
@@ -0,0 +1,83 @@
+package net.scrafet.service;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import net.scrafet.model.Categoria;
+
+@Service
+public class CategoriasServiceImpl implements ICategoriasService {
+	List<Categoria> lista = null;
+	
+	public CategoriasServiceImpl() {
+		lista =new LinkedList<Categoria>();
+		
+		try {
+			
+			//Categoria 1
+			Categoria categoria1 = new Categoria();
+			categoria1.setId(1);
+			categoria1.setNombre("Ventas");
+			categoria1.setDescripcion("Sector de Ventas");
+			
+			//Categoria 2
+			Categoria categoria2 = new Categoria();
+			categoria2.setId(2);
+			categoria2.setNombre("Contabilidad");
+			categoria2.setDescripcion("Sector Bancario , manejo de Cuentas bancarias,transacciones y estados de cuenta");
+			
+			//Categoria 3
+			Categoria categoria3 = new Categoria();
+			categoria3.setId(3);
+			categoria3.setNombre("Transporte");
+			categoria3.setDescripcion("Sector de tranporte de diversos vehiculos aereos, terretres y marinos");
+			
+			//Categoria 4
+			Categoria categoria4 = new Categoria();
+			categoria4.setId(4);
+			categoria4.setNombre("Informatica");
+			categoria4.setDescripcion("Sector de Tecnologia de la Informacion");
+			
+			//Categoria 5
+			Categoria categoria5 = new Categoria();
+			categoria5.setId(5);
+			categoria5.setNombre("Construccion");
+			categoria5.setDescripcion("Sector de diseño planeacion y ejecucion de obras civiles y empresariales");		
+			
+			lista.add(categoria1);
+			lista.add(categoria2);
+			lista.add(categoria3);
+			lista.add(categoria4);
+			lista.add(categoria5);
+			
+			
+			
+		} catch (Exception e) {
+			System.out.println("Error : " + e.getMessage());
+		}
+	}
+
+	@Override
+	public void guardar(Categoria categoria) {
+		lista.add(categoria);
+		
+	}
+
+	@Override
+	public List<Categoria> buscarTodas() {
+		return lista;
+	}
+
+	@Override
+	public Categoria buscarPorId(Integer idCategoria) {
+	for (Categoria c : lista) {
+		if (c.getId() == idCategoria) {
+			return c;
+		}
+	}
+		return null;
+	}
+
+}
